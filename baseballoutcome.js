ebe77b6316ac4e80f6e8e957b1bbd5aeeadbbcc8
Walter <wzavala@clearspringrx.com>
2024-10-29 09:54:11 -0600

    Full three phrases working

--- baseballoutcome.js
+++ baseballoutcome.js
@@ -3,22 +3,30 @@ const firstPhrase = ['Strikeout', 'Groundball', 'Flyball', 'Single', 'Double', '
 
 const secondPhrase = ['pitcher', 'catcher', 'first baseman', 'second baseman', 'third baseman', 'shortstop', 'left fielder', 'center fielder', 'right fielder', 'left field', 'center field', 'right field', 'looking', 'swinging'];
 
-const thirdPhrase = ['Good job!', 'Better luck next time!'];
+const thirdPhrase = ['good job!', 'better luck next time!'];
 
 //Random word generator
 const predictBaseballOutcome = () => {
     const play = firstPhrase[Math.floor(Math.random()*firstPhrase.length)];
-    
-    const option = '';
+    let option = '';
+    let feedback = '';
+
     if (play === 'Strikeout') {
-        return option = secondPhrase[Math.floor(Math.random()+13)]
+        option = ` ${secondPhrase[Math.floor(Math.random()+13)]}`
     } else if (play === 'Homerun') {
-        return option = secondPhrase[Math.floor(Math.random()*2+10)]
+        option = ` to ${secondPhrase[Math.floor(Math.random() * 2 + 10)]}`
     } else {
-        return option = secondPhrase[Math.floor(Math.random)*9]
+        option = ` to the ${secondPhrase[Math.floor(Math.random() * 9)]}`
     };
-
-    const feedback = ''
-    if (play )
-
+    
+    if (play === 'Strikeout' || play === 'Groundball' || play === 'Flyball') {
+        feedback = `, ${thirdPhrase[1]}`
+    } else {
+        feedback = `, ${thirdPhrase[0]}`;
+    }
+    
+    const fullPhrase = play + option + feedback;
+    return fullPhrase;
 }
+
+console.log(predictBaseballOutcome());
